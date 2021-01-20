69ae9fe9a7c4fbe4db6bebb7e9ab91dcbe4f1b2f
Beomsoo <k1a11220@noreply.github.com>
2021-01-21 07:16:45 +0900

    Update data.py
    To solve invaild syntax error. {condition}.csv

--- data.py
+++ data.py
@@ -12,12 +12,13 @@ totals_df = totals_df.rename(columns={"index": "condition"})
 countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
 countries_df = countries_df.groupby("Country_Region").sum().reset_index()
 
+
 def make_country_df(country):
     def make_df(condition):
-        df = pd.read_csv(f"data/time_{condition}.csv")
+        df = pd.read_csv("data/time_confirmed.csv")
         df = df.loc[df["Country/Region"] == country]
         df = (
-            df.drop(columns = ["Province/State", "Country/Region", "Lat", "Long"])
+            df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"])
             .sum()
             .reset_index(name=condition)
         )
@@ -33,22 +34,23 @@ def make_country_df(country):
             final_df = final_df.merge(condition_df)
     return final_df
 
-def make_global_df():
-    def make_df(condition):
-        df = pd.read_csv(f"data/time_{condition}.csv")
-        df = (
-            df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
-            .sum()
-            .reset_index(name=condition)
-        )
-        df = df.rename(columns={"index": "date"})
-        return df
 
-    final_df = None
-    for condition in conditions:
-        condition_df = make_df(condition)
-        if final_df is None:
-            final_df = condition_df
-        else:
-            final_df = final_df.merge(condition_df)
-    return final_df
+# def make_global_df():
+#     def make_df(condition):
+#         df = pd.read_csv(f"data/time_{condition}.csv")
+#         df = (
+#             df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
+#             .sum()
+#             .reset_index(name=condition)
+#         )
+#         df = df.rename(columns={"index": "date"})
+#         return df
+
+#     final_df = None
+#     for condition in conditions:
+#         condition_df = make_df(condition)
+#         if final_df is None:
+#             final_df = condition_df
+#         else:
+#             final_df = final_df.merge(condition_df)
+#     return final_df
