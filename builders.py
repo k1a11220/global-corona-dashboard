3d3ae428323acbf8169c685b713d17c9fbde57ab
Beomsoo <k1a11220@noreply.github.com>
2021-01-23 19:43:52 +0900

    Add map && graph
    Add map && graph

--- builders.py
+++ builders.py
@@ -5,10 +5,37 @@ def make_table(df):
     return html.Table(
         children=[
             html.Thead(
-                children=[html.Tr(children=[html.Th(column_name.replace("_", " ")) for column_name in df.columns])]
+                style={"display": "block", "marginBottom": 25},
+                children=[
+                    html.Tr(
+                        children=[html.Th(column_name.replace("_", " ")) for column_name in df.columns],
+                        style={
+                            "display": "grid",
+                            "gridTemplateColumns": "repeat(4, 1fr)",
+                            "fontWeight": "600",
+                            "fontSize": 16,
+                        },
+                    )
+                ],
             ),
             html.Tbody(
-                children=[html.Tr(children=[html.Td(value_column) for value_column in value]) for value in df.values]
+                style={
+                    "maxHeight": "50vh",
+                    "display": "block",
+                    "overflow": "scroll",
+                },
+                children=[
+                    html.Tr(
+                        style={
+                            "display": "grid",
+                            "gridTemplateColumns": "repeat(4, 1fr)",
+                            "border-top": "1px solid white",
+                            "padding": "30px 0px",
+                        },
+                        children=[html.Td(value_column, style={"textAlign": "center"}) for value_column in value],
+                    )
+                    for value in df.values
+                ],
             ),
-        ]
+        ],
     )
--- main.py
+++ main.py
@@ -2,15 +2,46 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-from data import countries_df
+from data import countries_df, totals_df
 from builders import make_table
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
-    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
+    "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap",
 ]
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
+
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    hover_name="Country_Region",
+    color="Confirmed",
+    locations="Country_Region",
+    locationmode="country names",
+    size_max=40,
+    title="Confirmed By Country",
+    template="plotly_dark",
+    color_continuous_scale=px.colors.sequential.Oryel,
+    hover_data={
+        "Confirmed": ":,",
+        "Deaths": ":,",
+        "Recovered": ":,",
+        "Country_Region": False,
+    },
+)
+bubble_map.update_layout(margin=dict(l=0, r=0, t=50, b=0))
+
+bars_graph = px.bar(
+    totals_df,
+    x="condition",
+    hover_data={"count": ":,"},
+    y="count",
+    template="plotly_dark",
+    title="Total Global Cases",
+    labels={"condition": "Condition", "count": "Count", "color": "Condition"},
+)
+bars_graph.update_traces(marker_color=["#e74c3c", "#8e44ad", "#27ae60"])
 
 app.layout = html.Div(
     style={
@@ -21,10 +52,33 @@ app.layout = html.Div(
     },
     children=[
         html.Header(
-            style={"textAlign": "center", "paddingTop": "50px"},
+            style={"textAlign": "center", "paddingTop": "50px", "marginBottom": 100},
             children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
         ),
-        html.Div(children=[html.Div(children=[make_table(countries_df)])]),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[
+                html.Div(
+                    style={"grid-column": "span 3"},
+                    children=[dcc.Graph(figure=bubble_map)],
+                ),
+                html.Div(children=[make_table(countries_df)]),
+            ],
+        ),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[
+                html.Div(children=[dcc.Graph(figure=bars_graph)]),
+            ],
+        ),
     ],
 )
 
